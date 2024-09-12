52e8fab5c0a36c52b78f0d218fe218d9cc155850
dudeforlifezYT-pixel <kellenshort112@gmail.com>
2024-09-12 09:05:15 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Magic Tree",
+	id: "40068176",
+	author: "Dudeforlifez",
+	pointsName: "Magic Points",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
